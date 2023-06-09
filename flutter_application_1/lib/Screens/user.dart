af20633891f7f722c66b6616f6e151ae5da558a0
Wissaall <lw_messikh@esi.dz>
2023-06-09 15:34:39 +0200

    oop

---
+++ flutter_application_1/lib/Screens/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String username;
+  String password;
+  List<String> tanksList;
+  String phoneNumber;
+  String location;
+
+  User({
+    required this.username,
+    required this.password,
+    required this.tanksList,
+    required this.phoneNumber,
+    required this.location,
+  });
+}
